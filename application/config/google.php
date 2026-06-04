65cfe1e4aa03046369c5d623bd08679fa61baadf
Depri Kurnia <42798521+depri12kurnia@users.noreply.github.com>
2026-06-04 20:26:23 +0700

    done

--- application/config/google.php
+++ application/config/google.php
@@ -1,6 +1,10 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-$config['google']['client_id']     = 'xxxxxxxxxxxxxxxxxxx.apps.googleusercontent.com';
-$config['google']['client_secret'] = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxx';
-$config['google']['redirect_uri']  = base_url('auth/google_callback');
+// config google.php
+
+
+// Palsu, ganti dengan kredensial Google OAuth yang valid dari akun Google Anda
+// $config['google']['client_id']     = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.apps.googleusercontent.com';
+// $config['google']['client_secret'] = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+// $config['google']['redirect_uri']  = base_url('auth/google_callback');
--- application/config/qontak.php
+++ application/config/qontak.php
@@ -6,6 +6,9 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | Konfigurasi API Mekari Qontak
 | -------------------------------------------------------------------
 */
+// Palsu, ganti dengan token akses dan ID saluran yang valid dari akun Qontak Anda
+// $config['qontak_access_token'] = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+// $config['qontak_channel_id']   = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
 
-$config['qontak_access_token'] = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
-$config['qontak_channel_id']   = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+// Config qontak.php
+// Token API Aplikasi Penerimaan Tamu
